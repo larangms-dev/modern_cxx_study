744b59793e4bca8a36d3c11a239becd05f529d66
yhoney <chaeyh04@gmail.com>
2023-02-03 09:05:17 +0900

    로마자 9000 수정

--- cyh/function_test/test.cpp
+++ cyh/function_test/test.cpp
@@ -23,12 +23,22 @@ TEST(function, gray_code)
 
 	UINT64 n_binary = Function::GrayCodeToBinary(n_gray_code);
 	EXPECT_EQ(n_binary, 0b1001);
+
+	n = 0b111101010101;
+	n_gray_code = Function::BinaryToGrayCode(n);
+	EXPECT_EQ(n_gray_code, 0b100011111111);
+
+	n_binary = Function::GrayCodeToBinary(n_gray_code);
+	EXPECT_EQ(n_binary, 0b111101010101);
 }
 
 TEST(function, exponent)
 {
 	UINT64 n = pow(2, 15);
 	EXPECT_EQ(Function::Exponent(n), 15);
+
+	n = 1 << 7;
+	EXPECT_EQ(Function::Exponent(n), 7);
 }
 
 TEST(function, bit)
@@ -43,5 +53,6 @@ TEST(function, roma_number)
 {
 	EXPECT_EQ(Function::RomaNumber(666), L"DCLXVI");
 	EXPECT_EQ(Function::RomaNumber(777), L"DCCLXXVII");
+	EXPECT_EQ(Function::RomaNumber(888), L"DCCCLXXXVIII");
 	EXPECT_EQ(Function::RomaNumber(999), L"CMXCIX");
 }
