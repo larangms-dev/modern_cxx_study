7577f41eb840f60beb71811b4f0d8bb5efd75056
yhoney <yhoney@estsecurity.com>
2023-02-08 18:25:33 +0900

    설명 주석 추가

--- cyh/function_test/function.cpp
+++ cyh/function_test/function.cpp
@@ -1,4 +1,4 @@
-#include <cmath>
+﻿#include <cmath>
 
 #include "pch.h"
 #include "function.h"
@@ -6,6 +6,18 @@
 vector<UINT64> Function::PrimeFactorization(UINT64 n)
 {
 	vector<UINT64> answer;
+
+	// n의 제곱근을 구하는 이유는
+	// 10을 예로들면 10의 제곱근은 대략 3이다 (9의 제곱근이 3이며 16의 제곱근이 4)
+	// 따라서 3보다 작은 수의 배수를 거르면
+	//		2, 4, 6, 8, 10
+	//		3, 6, 9
+	//		1(무시), 5, 7이되고
+	// 10에서 작은 값 부터 나눌 경우 (10 / 5 = 2)가 되며
+	// 몫인 2로 다시 반복을 수행할 경우 2의 제곱근은 1보다 작기에 for문을 수행하지 않고 바로 나누어 버린다.
+	//																				   (n /= n_mpn)
+	// 2 / 2 = 1이 되기에 while문이 종료된다.
+	// answer(5, 2)
 
 	UINT64 n_sqrt = floor(sqrt(n)), n_mpn = 0;
 
@@ -32,10 +44,13 @@ vector<UINT64> Function::PrimeFactorization(UINT64 n)
 UINT64 Function::BinaryToGrayCode(UINT64 n)
 {
 	UINT64 answer = 0, n_exponent = Exponent(n);
+	// 최상위 비트는 그대로 적용
 	answer |= 1ull << n_exponent;
 
+	// n의 i 번째 비트와 n의 i + 1 번째 비트(i 번째 비트보다 한칸 왼쪽에 있는)와 XOR 연산
 	for (UINT64 i = 0; i < n_exponent; i++)
 	{
+		// XOR 연산 결과가 i 번째 비트에 반영되어야 하므로 i만큼 왼쪽 쉬프트 연산 수행
 		answer |= (Bit(n, i) ^ Bit(n, i + 1)) << i;
 	}
 
@@ -45,13 +60,17 @@ UINT64 Function::BinaryToGrayCode(UINT64 n)
 UINT64 Function::GrayCodeToBinary(UINT64 n)
 {
 	UINT64 answer = 0, n_exponent = Exponent(n);
+	// 최상위 비트는 그대로 적용
 	answer |= 1ull << n_exponent;
 
+	// answer의 i + 1 번째 비트(i 번째 비트보다 한칸 왼쪽에 있는)와 n의 i 번째 비트와 XOR 연산
 	for (UINT64 i = n_exponent - 1; i > 0; i--)
 	{
+		// XOR 연산 결과가 i 번째 비트에 반영되어야 하므로 i만큼 왼쪽 쉬프트 연산 수행
 		answer |= (Bit(answer, i + 1) ^ Bit(n, i)) << i;
 	}
 
+	// for문의 조건식을 i >= 0으로 할 경우 무한 반복이 됨
 	answer |= (Bit(answer, 1) ^ Bit(n, 0));
 
 	return answer;
@@ -61,6 +80,8 @@ UINT64 Function::Exponent(UINT64 n)
 {
 	UINT64 answer = 0;
 
+	// 최상위 비트가 몇 번째 비트인지 확인
+	// n을 오른쪽으로 1칸씩 밀면서 1이상이면 반복
 	while (n = n >> 1)
 	{
 		++answer;
@@ -71,6 +92,7 @@ UINT64 Function::Exponent(UINT64 n)
 
 UINT64 Function::Bit(UINT64 base, UINT64 index)
 {
+	// base에 대해서 1을 index만큼 왼쪽으로 밀었을 때 그 비트가 1인지 0인지 확인
 	return (base & (1ull << index) ? 1 : 0);
 }
 
@@ -94,19 +116,31 @@ wstring Function::RomaNumber(UINT64 n)
 		{ L"IX",     9 },        { L"V",  5 },        { L"IV",   4 },        { L"I",  1 },
 	};
 
+	// 몫
 	double share = 0;
+	// 나머지
 	UINT64 remainder = 0;
+	// 제일 큰 수 부터 연산을 수행
 	for (auto& iter : romans)
 	{
+		// 몫 구하기
 		share = n / iter.value;
+		// 나머지 구하기
 		remainder = n % iter.value;
+		// 나머지 대입
+		// 12 % 90000000 = 12
+		// 12 % 10 = 2
 		n = remainder;
 
+		// 몫이 1보다 크다면 몫 만큼 반복하면서 나누었던 수의 로마자를 추가한다.
+		// 12(XII) / 10(X) = 1.2
+		// answer(X)
 		for (UINT64 j = 0; j < share; j++)
 		{
 			answer += iter.roman;
 		}
 
+		// 나머지가 0이면 종료
 		if (n == 0)
 		{
 			break;
